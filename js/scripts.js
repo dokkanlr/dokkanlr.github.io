cacf056de672df8e99629a3b0ad6b1002de1c0bf
antonlabz <20198370+antonlabz@users.noreply.github.com>
2019-12-27 20:24:08 +1100

    lucy 6+

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,7 @@ var base = [
   {base: 1747, evo: 2434},
   {base: 1751, evo: 1922},
   {base: 1763, evo: 2784},
+  {base: 1794, evo: 2814},
   {base: 1832, evo: 2138},
   {base: 1869, evo: 2505},
   {base: 1935, evo: 2300},
