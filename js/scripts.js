791f31c9738db6ac2f2cbebd5aff8a23de8ea77f
antonlabz <20198370+antonlabz@users.noreply.github.com>
2021-12-17 10:39:53 +1100

    brook 6+, kaido crew

--- js/scripts.js
+++ js/scripts.js
@@ -54,6 +54,7 @@ var base = [
   {base: 2651, evo: 2681},
   {base: 2686, evo: 2909},
   {base: 2148, evo: 3211},
+  {base: 2236, evo: 3510},
   {base: 2338, evo: 3417},
   {base: 2441, evo: 3393},
   {base: 2465, evo: 3298},
