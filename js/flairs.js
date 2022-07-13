8dba8969b3d2409686edde9abf44146e8461bec5
antonlabz <20198370+antonlabz@users.noreply.github.com>
2022-07-13 17:14:53 +1000

    up to trunks/vegeta

--- js/flairs.js
+++ js/flairs.js
@@ -6,7 +6,7 @@ loadPage = function() {
 	loadSpecial = function() {
 		var enter = document.getElementById('special');
 
-		for (i=1; i<=97; i++) {
+		for (i=1; i<=102; i++) {
 			//creates HTML for special flairs
 			var flair_special = document.createElement('img');
 			flair_special.setAttribute('class', 'flair');
