4888cde5306897c892aab06daa39d24f7cd50d7a
antonlabz <20198370+antonlabz@users.noreply.github.com>
2023-01-30 14:49:30 +1100

    Updated GET URL for proper github API compatible with CORS

--- js/flairs.js
+++ js/flairs.js
@@ -1,12 +1,12 @@
 var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://github.com/dokkan-lr/dokkan-lr.github.io/tree/master/images/icons", true);
+xhr.open("GET", "https://api.github.com/repos/dokkan-lr/dokkan-lr.github.io/contents/images/icons?ref=master", true);
 
 xhr.onload = function() {
 	if (xhr.readyState === xhr.DONE) {
 		if (xhr.status === 200) {
 			// match strictly against the XML containing the file URL elements in server directory
-			var n = (xhr.response.match(/<script>addRow\("[0-9]{1,3}.png/g).length);
+			var n = (xhr.response.match(/"name": "[0-9]{1,3}.png/g).length);
 
 			var enter = document.getElementById('special');
 
