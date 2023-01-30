de8bddad866054e6e0e3f780b796d23171bc1e4a
antonlabz <20198370+antonlabz@users.noreply.github.com>
2023-01-30 16:03:06 +1100

    Removed flairs.js and migrated code to scripts.js
    The flair code was migrated to the top of scripts.js to ensure that the functionality code was underneath, in order to fix a bug where event handlers were not correctly added to each generated flair

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+// create icon elements based on server directory file count
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.github.com/repos/dokkan-lr/dokkan-lr.github.io/contents/images/icons?ref=master", true);
+
+xhr.onload = function() {
+	if (xhr.readyState === xhr.DONE) {
+		if (xhr.status === 200) {
+			// match strictly against the XML containing the file URL elements in server directory
+			var n = (xhr.response.match(/"name": "[0-9]{1,3}.png/g).length);
+
+			var enter = document.getElementById('special');
+
+			// loop to generate flairs based on file count
+			for (var i = 1; i <= n; i++) {
+				var flair_special = document.createElement('img');
+				flair_special.setAttribute('class', 'flair');
+				flair_special.setAttribute('id', i);
+				flair_special.setAttribute('src', 'images/icons/'+i+'.png');
+				enter.appendChild(flair_special);
+			}
+		}
+	}
+};
+xhr.send();
+
+// updates local storage values
 function updateStorage(key, value, save) {
   if (save) {
     localStorage.setItem(key, value);
@@ -7,10 +34,12 @@ function updateStorage(key, value, save) {
   }
 }
 
+// reads local storage based on key
 function readStorageValue(key) {
   return localStorage.getItem(key);
 }
 
+// reads entire local storage array
 function readAllStorage() {
   const nbItem = localStorage.length;
   const store = [];
@@ -26,6 +55,7 @@ function readAllStorage() {
   return store;
 }
 
+// reads storage and updates the page accordingly
 function updatePage() {
   //check local storage
   const store = readAllStorage();
@@ -43,6 +73,7 @@ function updatePage() {
   });
 }
 
+// select all icons on page
 function selectPage() {
     //adds selected class to every icon
     $("#special .flair:not(.disabled)").addClass("selected");
@@ -63,6 +94,7 @@ function selectPage() {
     }
 }
 
+// remove all selections on page
 function resetPage() {
   //check local storage
   const store = readAllStorage();
