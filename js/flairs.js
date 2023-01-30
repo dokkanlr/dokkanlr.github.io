ba5d7aa363e046fe52554f444412ade640ea32d1
antonlabz <20198370+antonlabz@users.noreply.github.com>
2023-01-30 14:11:22 +1100

    8th anni duo, replaced code for XML request  to get file count

--- js/flairs.js
+++ js/flairs.js
@@ -1,23 +1,24 @@
-loadPage = function() {
+var xhr = new XMLHttpRequest();
 
-	var flair_class = '';
+xhr.onload = function() {
+	if (xhr.readyState === xhr.DONE) {
+		if (xhr.status === 200) {
+			// match strictly against the XML containing the file URL elements in server directory
+			var n = (xhr.response.match(/<script>addRow\("[0-9]{1,3}.png/g).length);
 
-	//SPECIAL FLAIRS
-	loadSpecial = function() {
-		var enter = document.getElementById('special');
+			var enter = document.getElementById('special');
 
-		for (i=1; i<=112; i++) {
-			//creates HTML for special flairs
-			var flair_special = document.createElement('img');
-			flair_special.setAttribute('class', 'flair');
-			flair_special.setAttribute('id', i);
-			flair_special.setAttribute('src', 'images/icons/'+i+'.png');
-
-			enter.appendChild(flair_special);
+			// loop to generate flairs based on file count
+			for (var i = 1; i <= n; i++) {
+				var flair_special = document.createElement('img');
+				flair_special.setAttribute('class', 'flair');
+				flair_special.setAttribute('id', i);
+				flair_special.setAttribute('src', 'images/icons/'+i+'.png');
+				enter.appendChild(flair_special);
+			}
 		}
 	}
-	loadSpecial();
-}
+};
 
-//waits for DOM to load before executing function
-document.addEventListener('DOMContentLoaded', loadPage, false);
+xhr.open("GET", "images/icons/", true);
+xhr.send();
