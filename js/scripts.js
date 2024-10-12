ed68d7236ffbd043db5f888d905202b9d792a69b
antonlabz <20198370+antonlabz@users.noreply.github.com>
2024-10-12 20:13:22 +1100

    gohan/piccolo

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ loadFlairs = function() {
     10,11,12,13,14,15,16,17,18,19,
     20,21,22,23,24,25,26,27,28,29,
     30,31,32,33,34,35,36,37,38,39,
-    40,41,42,43,44,45,46,47,49,
+    40,41,42,43,44,45,46,47,48,49,
     50,51,52,53,54,55,56,57,58,59,
     60,61,62,63,64,65,67,68,69,
     70,71,72,74,75,76,77,
@@ -18,18 +18,20 @@ loadFlairs = function() {
   ]
 
   let eza2 = [
-
+    1
   ]
 
   //changelog items
   const updateItems = [
-  "TEQ Gogeta EZA",
-  "PHY Janemba EZA"
+  "TEQ Gohan/Piccolo",
+  "TEQ SSJ Goku Super EZA",
+  "TEQ Demon King Piccolo EZA",
+  "TEQ Grandpa Gohan"
   ]
 
   //changelog date
   const options = {year: 'numeric', month: 'long', day: 'numeric'};
-  const d = new Date("2024-10-07");
+  const d = new Date("2024-10-12");
 
   //append formatted date
   $('.changelog h3').append("Last Update: "+d.toLocaleDateString("en-GB", options));
